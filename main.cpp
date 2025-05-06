8453c265014d77c4e60f8e13d3fc025871dac016
Jerxmyz <jjzambrano15@espe.edu.ec>
2025-05-05 20:50:49 -0500

    Correccion del CAP2
    Capitulo 2 codigo corregido

--- main.cpp
+++ main.cpp
@@ -2,41 +2,37 @@
 
 using namespace std;
 
-class AlmacenamientoNumeros {   // Nombre descriptivo
-    int numeros[10];             // Arreglo para almacenar números
-    int contador = 0;            // Contador de números almacenados
-
+class AlmacenamientoNumeros {   // Nombre más descriptivo
+    int numeros[10];             // Cambiado de "a" a "numeros"
+    int contador = 0;            // Cambiado de "i" a "contador"
 public:
-    void agregarNumero(int valor) { // Agrega un número al arreglo
-        if (contador < 10) {        // Verifica que no se exceda el tamaño
-            numeros[contador++] = valor;
-        } else {
-            cout << "Capacidad máxima alcanzada." << endl;
-        }
+    void agregarNumero(int valor) { // Cambiado de "x" a "agregarNumero"
+        numeros[contador++] = valor;
     }
     
-    int calcularSuma() const {      // Calcula la suma de los números
-        int suma = 0;                // Variable para almacenar la suma
-        for (int indice = 0; indice < contador; ++indice) {
+    int calcularSuma() {          // Cambiado de "y" a "calcularSuma"
+        int suma = 0;             // Cambiado de "s" a "suma"
+        for (int indice = 0; indice < contador; ++indice) { // Cambiado de "j" a "indice"
             suma += numeros[indice];
         }
         return suma;
     }
     
-    double calcularPromedio() const { // Calcula el promedio de los números
+    double calcularPromedio() {   // Cambiado de "z" a "calcularPromedio"
         return contador == 0 ? 0.0 : static_cast<double>(calcularSuma()) / contador;
     }
 };
 
 int main() {
-    AlmacenamientoNumeros almacenamiento; // Objeto para almacenar números
-    int totalNumeros;                   // Total de números a ingresar
-    cout << "¿Cuántos números ingresará? ";
+    setlocale(LC_ALL, "Spanish"); 
+    AlmacenamientoNumeros almacenamiento; // Cambiado de "p" a "almacenamiento"
+    int totalNumeros;                   // Cambiado de "t" a "totalNumeros"
+    cout << "¿Cuantos numeros ingresara? ";
     cin >> totalNumeros;
     
-    for (int indice = 0; indice < totalNumeros; ++indice) {
-        cout << "Número: ";
-        int numero;                     // Variable para el número ingresado
+    for (int indice = 0; indice < totalNumeros; ++indice) { // Cambiado de "k" a "indice"
+        cout << "Numero: ";
+        int numero;                     // Cambiado de "d" a "numero"
         cin >> numero;
         almacenamiento.agregarNumero(numero);
     }
